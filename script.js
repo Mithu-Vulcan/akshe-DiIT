1dbe9ba165ed01b2db14ec005b3bd92ac947a879
Mithu-Vulcan <mithushanthjonathanparrot@gmail.com>
2025-07-11 09:17:50 +0530

    added footer

--- script.js
+++ script.js
@@ -22,7 +22,9 @@ class navSection extends HTMLElement {
 
 		let navHTML = "";
 		config.navItems.forEach((element) => {
-			navHTML += `<li><a href="${element.link}">${element.label}</a></li>`;
+			navHTML += `<li><a href="${element.link}" target="${
+				element.target ?? "_self"
+			}">${element.label}</a></li>`;
 		});
 
 		const containerHtml = `
@@ -47,3 +49,61 @@ class navSection extends HTMLElement {
 }
 
 customElements.define("nav-section", navSection);
+
+class footerSection extends HTMLElement {
+	connectedCallback() {
+		if (window.siteConfig) {
+			this.render();
+		} else {
+			document.addEventListener("configLoaded", () => this.render(), {
+				once: true,
+			});
+		}
+	}
+
+	render() {
+		const config = window.siteConfig;
+		if (!config || !config.navItems) return;
+
+		let footerNavHtml = "";
+		config.navItems.forEach((element) => {
+			footerNavHtml += `<p class="footer-body"><a href="${
+				element.link
+			}" target=${element.target ?? "_self"}>${element.label}</a></p>`;
+		});
+
+		const containerHtml = `
+			<section class="footer" id="footer">
+			<img src="assets/footer-bg-cropped.webp" alt="footer-bg-cropped" />
+			<div class="footer-texts">
+				<div class="footer-1">
+					<p class="footer-logo">${config.siteName}</p>
+				</div>
+				<div class="footer-2">
+					<p class="footer-head">About Us</p>
+					<p class="footer-body">Phone: ${config.phone}</p>
+					<p class="footer-body">Email: ${config.email}</p>
+					<p class="footer-body">
+						Address: ${config.address}
+					</p>
+				</div>
+                <div class="footer-3">
+					<p class="footer-head">Explore More</p>
+                    ${footerNavHtml}
+                </div>
+			</div>
+		</section>
+		`;
+
+		let temp = document.createElement("div");
+		temp.innerHTML = containerHtml;
+
+		const parent = this.parentNode;
+		while (temp.firstChild) {
+			parent.insertBefore(temp.firstChild, this);
+		}
+		parent.removeChild(this);
+	}
+}
+
+customElements.define("footer-section", footerSection);
